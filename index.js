d40443a7f21f65b4e7628cffcb87fce1a70625b2
AnandKrishnarajuN <anandanu9897@gmail.com>
2025-05-24 20:42:52 +0530

    third input

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 function calculate() {
     const firstNumber = Number(document.getElementById("num1").value);
     const secondNumber = Number(document.getElementById("num2").value);
+    const thirdNumber = Number(document.getElementById("num2").value);
     const operator = document.getElementById("operator").value;
     let output;
     switch (operator) {
         case "-":
-            output = firstNumber - secondNumber;
+            output = firstNumber - secondNumber - thirdNumber;
             break;
         case "+":
-            output = firstNumber + secondNumber;
+            output = firstNumber + secondNumber + thirdNumber;
             break;
         case "*":
-            output = firstNumber * secondNumber;
+            output = firstNumber * secondNumber * thirdNumber;
             break;
         case "/":
             output = firstNumber / secondNumber;
